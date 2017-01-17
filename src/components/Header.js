420e1a88ee306fdf29d17133cebc4491896ccd38
Erik Davtyan <erikdavtyan@gmail.com>
2017-01-17 18:55:59 +0400

    tooling

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div id='header'>
+        Home
+      </div>
+    )
+  }
+}
+
+export default Header;
