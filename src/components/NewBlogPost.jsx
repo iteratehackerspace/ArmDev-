47c70baca49a454aee195dc5100f86f533d07360
Erik Davtyan <erikdavtyan@gmail.com>
2017-01-20 15:51:03 +0400

    new blog post styling

--- src/components/NewBlogPost.jsx
+++ src/components/NewBlogPost.jsx
@@ -3,17 +3,17 @@ import '../css/NewBlogPost.css'
 
 class NewBlogPost extends Component {
   _handleNewBlogPost(e) {
-    e.defaultPrevented;
+    e.preventDefault();
     const tagsArray = this._tags.value.split(' ');
     this.props.handleNewBlogPost(this._title.value, tagsArray, this._text.value);
   }
   render() {
     return (
       <form onSubmit={this._handleNewBlogPost.bind(this)}>
-        <input placeholder='Blog name:' ref={input => this._title = input}/>
-        <input placeholder='Blog tags:' ref={input => this._tags = input}/>
-        <textarea rows='10' cols='50' placeholder='Blog text:' ref={textarea => this._text = textarea}/>
-        <input type='submit' value='Submit Blog'/>
+        <input className='textBox' placeholder='Blog name:' ref={input => this._title = input}/>
+        <input className='textBox' placeholder='Blog tags:' ref={input => this._tags = input}/>
+        <textarea className='textBox' rows='10' cols='50' placeholder='Blog text:' ref={textarea => this._text = textarea}/>
+        <input className='submitButton' type='submit' value='Submit Blog'/>
       </form>
     )
   }
