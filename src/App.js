76eb4d13bb856173a1e60f337c5c420ede3dc832
Erik Davtyan <erikdavtyan@gmail.com>
2017-01-21 15:01:28 +0400

    hide or show new post box

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import NewBlogPost from './components/NewBlogPost.jsx';
 import AllBlogPosts from './components/AllBlogPosts.jsx';
 import FrontPage from './components/frontpage/FrontPage.jsx'
+// import LogInBox from './components/LogInBox.jsx';
+// import SignUpBox from './components/SignUpBox.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <FrontPage />
+        <AllBlogPosts />
       </div>
     );
   }
--- src/components/NewBlogPost.jsx
+++ src/components/NewBlogPost.jsx
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import '../css/NewBlogPost.css'
 
 class NewBlogPost extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isShown: true,
+    }
+  }
+  showHide() {
+    this.setState({isShown: !this.state.isShown});
+  }
   _handleNewBlogPost(e) {
     e.preventDefault();
     const tagsArray = this._tags.value.split(' ');
     this.props.handleNewBlogPost(this._title.value, tagsArray, this._text.value);
   }
   render() {
-    return (
-      <form onSubmit={this._handleNewBlogPost.bind(this)}>
-        <input className='textBox' placeholder='Blog name:' ref={input => this._title = input}/>
-        <input className='textBox' placeholder='Blog tags:' ref={input => this._tags = input}/>
-        <textarea className='textBox' rows='10' cols='50' placeholder='Blog text:' ref={textarea => this._text = textarea}/>
-        <input className='submitButton' type='submit' value='Submit Blog'/>
-      </form>
+    const showOrHide = () => {
+      if (this.state.isShown) {
+        return (
+            <form onSubmit={this._handleNewBlogPost.bind(this)}>
+              <input className='textBox' placeholder='Blog name:' ref={input => this._title = input}/>
+              <input className='textBox' placeholder='Blog tags:' ref={input => this._tags = input}/>
+              <textarea className='textBox' rows='10' cols='50' placeholder='Blog text:' ref={textarea => this._text = textarea}/>
+              <input className='submitButton' type='submit' value='Submit Blog'/>
+              <button className='submitButton' onClick={this.showHide.bind(this)}>Hide</button>
+            </form>
+        )
+      }
+      else return <button className='submitButton' onClick={this.showHide.bind(this)}>Write a post</button>
+    }
+    return(
+      <div className='container'>
+        {showOrHide()}
+      </div>
     )
   }
 }
