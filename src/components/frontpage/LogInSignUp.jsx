3dc77160e8664fbee46cb2623b9155ff4f24f90a
root <root@localhost.localdomain>
2017-02-07 08:59:31 -0500

    add seperate logInSignUp css file

--- src/components/frontpage/LogInSignUp.jsx
+++ src/components/frontpage/LogInSignUp.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import LogInBox from './LogInBox.jsx';
 import SignUpBox from './SignUpBox.jsx';
 
+import '../../css/frontpage/logInSignUp.css'
+
 class LogInSignUp extends Component {
   render() {
     return (
